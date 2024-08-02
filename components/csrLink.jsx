7fd8a46967deb8afbbe2ac7e219a4d7ec5174e24
baji-web <camrynchelsea01@gmail.com>
2024-08-02 23:05:35 +0800

    added telegram link

--- components/csrLink.jsx
+++ components/csrLink.jsx
@@ -1,14 +1,21 @@
 import Link from 'next/link';
 import { FaFacebookMessenger } from "react-icons/fa6";
+import { FaTelegram } from "react-icons/fa6";
 
 export default function MessengerLink() {
     return (
         <div className='flex flex-col mx-auto items-center'>
-            <Link href="https://m.me/albert.andulte.5" passHref target="_blank" rel="noopener noreferrer"
-                className='my-4'>
-                <FaFacebookMessenger />
-            </Link>
-            <p>Chat us on Messenger</p> 
+            <div className='flex flex-row gap-4'>
+                <Link href="https://m.me/albert.andulte.5" passHref target="_blank" rel="noopener noreferrer"
+                    className='my-4'>
+                    <FaFacebookMessenger />
+                </Link>
+                <Link href="https://t.me/TEAMTGFCSR" passHref target="_blank" rel="noopener noreferrer"
+                    className='my-4'>
+                    <FaTelegram />
+                </Link>
+            </div>
+            <p>Chat Us</p>
         </div>
 
     );
