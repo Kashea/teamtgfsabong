c7e77fcf638082e952d27fbd4c78421cc75c09c8
baji-web <camrynchelsea01@gmail.com>
2024-08-12 21:37:33 +0800

    uploaded video background

--- app/page.js
+++ app/page.js
@@ -16,23 +16,34 @@ export default function Home() {
       <div className=" bg-gray-900 text-white">
         <header className="flex flex-col bg-gray-800 p-5 text-center">
           <h1 className="text-3xl font-bold">
-            Online Sabong Affiliate Program TEAM TGF
+            Online Sabong AGENT Program TEAM TGF
           </h1>
           <p className="mt-2">Join now and start earning commissions!</p>
           <Csr />
         </header>
 
         <main className="w-full mx-auto">
-          <section className="bg-[url('/banners/widebanner.webp')] bg-cover bg-left h-[371px] flex flex-col mx-auto items-center justify-center px-5">
-            <h2 className=" text-2xl font-semibold">
-              Why Join Our Affiliate Program?
-            </h2>
-            <p className="mt-4 text-lg">
-              Become a partner in the exciting world of online sabong. Enjoy
-              high commissions, timely payouts, and comprehensive marketing
-              support.  
-            </p>
+          <section className="relative h-[371px] flex flex-col items-center justify-center px-5">
+            <video
+              className="absolute top-0 left-0 w-full h-full object-cover"
+              src="/banners/sabongvid01.mp4"
+              autoPlay
+              loop
+              muted
+            />
+            <div className="relative z-10 text-center text-white">
+              <h2 className="text-2xl font-semibold">
+                Why Join Our AGENT Program?
+              </h2>
+              <p className="mt-4 text-lg">
+                Become a partner in the exciting world of online sabong. Enjoy
+                high commissions, timely payouts, and comprehensive marketing
+                support.
+              </p>
+            </div>
+            <div className="absolute inset-0 bg-black opacity-50"></div>
           </section>
+
           <article className="bg-[url('/banners/background03.webp')] bg-cover bg-center h-full">
             <section className="pt-6 text-center">
               <div className="flex flex-wrap justify-center mt-8">
@@ -63,7 +74,7 @@ export default function Home() {
                 />
                 <FeatureCard
                   title="2. Promote"
-                  description="Use your unique affiliate link to promote our platform."
+                  description="Use your unique agent link to promote our platform."
                 />
                 <FeatureCard
                   title="3. Earn"
@@ -73,7 +84,9 @@ export default function Home() {
             </section>
 
             <section className="pb-6 text-center">
-              <h2 className="text-2xl font-semibold">Ready to Start Earning?</h2>
+              <h2 className="text-2xl font-semibold">
+                Ready to Start Earning?
+              </h2>
               <p className="mt-4 text-lg">
                 Don't miss out on the opportunity to be part of something big.
               </p>
